2cf25e917513c92d198db0366b0dda14a3593a6c
SanthoshkumarECE <santhosh16022003@gmail.com>
2025-01-17 09:54:28 +0530

    added comment

--- backend/router/Mainroute.js
+++ backend/router/Mainroute.js
@@ -2,6 +2,7 @@ const express = require("express")
 const userRoute = require("../users/router/userRoute")
 const theaterRoute = require("../Theaters/router/theaterRoute")
 const router = express.Router()
+//this is the where route with endpoints for user and theater
 
 router.use("/user", userRoute)
 router.use("/theater",theaterRoute)
